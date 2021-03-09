ff231bcbc6db4d52ff3b62f06c5611240cfd5bf3
uo264254 <uo264254@uniovi.es>
2021-03-09 19:45:48 +0100

    casi acabados los servicios web básicos

--- restapi/api.js
+++ restapi/api.js
@@ -10,7 +10,7 @@ router.get("/users/list", async (req, res) => {
 	res.send(users)
 })
 
-//register a new user
+//Register a new user
 router.post("/users/add", async (req, res) => {
     let name = req.body.name;
     let email = req.body.email;
@@ -31,13 +31,11 @@ router.post("/users/add", async (req, res) => {
 // Get all locations
 router.get("/locations/list", async (req, res) => {
     const positions = await Location.find({}).sort('-_id') //Inverse order
-    positions.forEach(element => {
-        console.log(element.name);
-    });
 	res.send(positions)
 })
 
-//register a new location
+//Register a new location
+//It is compulsory to assing a location for a user by his/her email
 router.post("/locations/add", async (req, res) => {
     let longitud = req.body.longitud;
     let latitud = req.body.latitud;
@@ -47,7 +45,8 @@ router.post("/locations/add", async (req, res) => {
     //Check if the location is already in the db
     let location = await Location.findOne({ longitud: longitud,
                                          latitud: latitud,
-                                         fecha: fecha })
+                                         fecha: fecha,
+                                        email: email})
     if (location)
         res.send({error:"Error: This location is already registered"})
     else{
@@ -74,15 +73,80 @@ router.get("/locations/:email", async (req, res) => {
     
     let user = await User.find(criterio).sort('-_id') //Inverse order
     
-    console.log(user[0].locations);
-
     let locs = user[0].locations;
+        
+    locs = await Location.find({
+        '_id': { $in: locs}
+    }, function(err, locs){
+         console.log(locs);
+    });
 
-    const positions = await Location.findById(locs).sort('-_id') //Inverse order
-
-    res.send(positions);
-
+    res.send(locs);
 })
 
+// Get all users in a 
+router.get("/users/:latitud/:longitud/:radio/:fecha/:rangoSeconds", async (req, res) => {
+    let lat1 = req.params.latitud - req.params.radio;
+    let lat2 = parseInt(req.params.latitud) + parseInt(req.params.radio);
+    let long1 = req.params.longitud - req.params.radio;
+    let long2 = parseInt(req.params.longitud) + parseInt(req.params.radio);
+    let fechaMin = new Date(req.params.fecha);
+    let fechaMax = new Date(req.params.fecha);
+    fechaMin.setHours(fechaMin.getHours() - 1 )//req.params.rangoSeconds);
+    console.log("fechaMin hours: " + fechaMin.getHours());
+    fechaMax.setHours(fechaMax.getHours() + parseInt(1));
+    console.log("fechaMax hours: " + fechaMax.getHours());
+    let criterio = {   latitud: {
+                            $gt: lat1,
+                            $lt: lat2
+                        },  
+                        longitud: {
+                            $gt: long1,
+                            $lt: long2
+                        },
+                        fecha: {
+                            $gt: fechaMin,
+                            $lt: fechaMax
+                        }
+                    }
+    
+    let locations_ids = await Location.find(criterio).select('_id') //Inverse order
+    console.log("locations_ids:" + locations_ids)
+    let locs = []
+    locations_ids.forEach(element => {
+        locs.push(element._id)
+        console.log(element._id)   
+    }
+    )
+    
+    criterio = {locations : { $in: locs} }
+    let users = await User.find(criterio).sort('-_id') //Inverse order
+	res.send(users);
+})
+
+// Get all locations for a given email and date
+router.get("/locations/:email/:fecha", async (req, res) => {
+    let criterioUsuario = {email : req.params.email};
+    
+    let user = await User.find(criterioUsuario).sort('-_id') //Inverse order
+    
+    //locations for the user
+    let locs = user[0].locations;
+    
+    console.log(locs);
+
+    let criterioFecha = {fecha: req.params.fecha};
+    
+    let location = await Location.find(criterioFecha).sort('-_id') //Inverse order
+
+    locs.forEach(element => {
+        if(element == location)
+            res.send(element)
+    });
+
+    const position = await Location.findById(locs).sort('-_id') //Inverse order
+
+    res.send(position);
+})
 
 module.exports = router
