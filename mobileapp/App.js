78bfbee1f308993bb583b296b359395c79cb3192
UO264699 <Andresfdez9@gmail.com>
2021-03-10 14:18:12 +0100

    obtener ubicación del dispositivo

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,26 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component,useState, useEffect} from 'react';
+import { StyleSheet, Button,Text,View,Alert,TouchableOpacity } from 'react-native';
+import * as Location from "expo-location";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css';
 import './react-leaflet.css';
+import 'leaflet/dist/leaflet.css';
+
 
 export default function App() {
+
+  const [position, setPosition] = useState(null);
+
+  const getPosition = async () => {
+    try {
+      const { coords } = await Location.getCurrentPositionAsync({});
+      setPosition(coords);
+    } catch (error) {
+      console.log("getPosition -> error", error);
+      setPosition(null);
+    }
+  };
+
+  const entryPoint = async () => {
+    try {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status === "granted") {
+        getPosition();
+      }
+    } catch (error) {
+      console.log("getPermissionAndPosition -> error", error);
+    }
+  };
+
+  useEffect(() => {
+    entryPoint();
+  }, []);
+
   return (
-  
-    <MapContainer className= 'leaflet-container'  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+
+  <View style={styles.container}>
+{(position && (
+      <View>
+        
+        
+      
+        <View>
+   
+    <MapContainer className= 'leaflet-container'  center={[position.latitude,position.longitude]} zoom={20} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  <Marker position={[position.latitude,position.longitude]}>
     <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
+     Estoy aquí
     </Popup>
   </Marker>
 </MapContainer>
-    
+
+</View>
+
+
+
+      </View>
+
+     
+
+
+    )) || (
+      <View>
+        <Text>GPS Unavailable</Text>
+      </View>
+    )}
+
+
+</View>
   );
 }
 
