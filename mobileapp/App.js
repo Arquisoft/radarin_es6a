91db20d8854045cd0a2eba547eefeaecc9ea1d95
uo271060 <uo271060@uniovi.es>
2021-03-11 17:17:21 +0100

    Generado nuevo proyecto mobileapp con mayores opciones y geolocalización
    se ha regenerado un nuevo proyecto para móviles que  incluye un ejemplo con geolocalización

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,15 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import GetLocation from 'react-native-get-location';
+
+const App: () => React$Node = () => {
+
+  const getPosition = () => {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+    })
+      .then(location => {
+        console.log(location);
+      })
+      .catch(error => {
+        const { code, message } = error;
+        console.warn(code, message);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Bienvenido a Radarin App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Localizate" onPress={getPosition}></Button>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +58,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
