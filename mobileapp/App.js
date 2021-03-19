75e9e9c6847688bc498a75fa034786b468d3256f
uo271060 <uo271060@uniovi.es>
2021-03-19 08:33:24 +0100

    Ultima version mobileapp

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,22 +1,81 @@
 import React from 'react';
-import { Button, SafeAreaView, StyleSheet, View } from 'react-native';
-import { startBackgroundFunction, stopBackgroundFunction, setBackgroundInterval } from './scripts/Background'
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TapGestureHandler } from 'react-native-gesture-handler';
+import { Home, Profile, Geo } from './screens/BasicScreens'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const App: () => React$Node = () => {
+const Tabs = createBottomTabNavigator();
+const GeoStack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+
+const GeoStackScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.main}>
+    <GeoStack.Navigator>
+      <GeoStack.Screen name="Geolocalización" component={Geo} />
+    </GeoStack.Navigator>
+  );
+}
 
-      </View>
-      <View style={styles.buttons}>
-        <Button title="Iniciar" onPress={startBackgroundFunction} />
-        <Button title="Parar" onPress={stopBackgroundFunction} />
-        <Button title="3s" onPress={() => setBackgroundInterval(3)} />
-        <Button title="10s" onPress={() => setBackgroundInterval(10)} />
-      </View>
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Inicio" component={Home} />
+    </HomeStack.Navigator>
+  );
+}
+
+const ProfileStackScreen = () => {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="Perfil" component={Profile} />
+    </ProfileStack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Tabs.Navigator initialRouteName="Inicio">
+          <Tabs.Screen
+            name="Geo"
+            component={GeoStackScreen}
+            options={{
+              tabBarLabel: 'Geo',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="earth" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="Inicio"
+            component={HomeStackScreen}
+            options={{
+              tabBarLabel: 'Inicio',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="Perfil"
+            component={ProfileStackScreen}
+            options={{
+              tabBarLabel: 'Perfil',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="account" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tabs.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -34,5 +93,3 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   }
 });
-
-export default App;
