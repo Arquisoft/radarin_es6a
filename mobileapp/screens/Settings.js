02c7d70610f25f5c561de5dcd172b33ca5ef2d00
uo271060 <uo271060@uniovi.es>
2021-03-20 11:05:18 +0100

    Cambios en los ajustes

--- mobileapp/screens/Settings.js
+++ mobileapp/screens/Settings.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Switch, Text, View } from "react-native";
 import { ScreenContainer } from './components/ScreenContainer';
 
+
 export const Settings = ({ navigation }) => {
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const [isEnabled2, setIsEnabled2] = useState(false);
+    const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
+
     return (
         <ScreenContainer>
             <View style={styles.container}>
-                <Text style={styles.text}>Proximamente...</Text>
+                <View style={styles.line}>
+                    <Text style={styles.text}>Habilitar geolocalización</Text>
+                    <Switch
+                        style={styles.botton}
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        thumbColor={isEnabled ? "#7ca8f2" : "#f4f3f4"}
+                        onValueChange={toggleSwitch}
+                        value={isEnabled}
+                    />
+                </View>
+                <View style={styles.line}>
+                    <Text style={styles.text}>Habilitar notificaciones</Text>
+                    <Switch
+                        style={styles.botton}
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        thumbColor={isEnabled2 ? "#f5dd4b" : "#f4f3f4"}
+                        onValueChange={toggleSwitch2}
+                        value={isEnabled2}
+                    />
+                </View>
             </View>
         </ScreenContainer>
     );
@@ -14,13 +40,22 @@ export const Settings = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1
+    },
+    line: {
+        flexDirection: 'row',
+        alignItems: "flex-start",
         justifyContent: "center",
-        alignItems: "center"
+        padding: 10
     },
     text: {
-        textAlign: "center",
-        fontSize: 22,
+        flex: 5,
+        textAlign: "left",
+        fontSize: 20,
+        marginTop: 10
+    },
+    botton: {
+        flex: 2,
         marginTop: 10
     }
 });
