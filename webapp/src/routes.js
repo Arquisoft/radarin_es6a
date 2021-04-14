b86ca708a96b2feb3e8faf252a861dc88e8f1c2e
UO264699 <Andresfdez9@gmail.com>
2021-04-14 15:21:39 +0200

    enviar mensajes y obtener mensajes

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -2,13 +2,18 @@ import React, { Fragment } from 'react';
 import { PrivateLayout, PublicLayout, NotLoggedInLayout } from '@layouts';
 import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 
-import { Login, Register, PageNotFound, Welcome, RegistrationSuccess } from './containers';
+import { Login, Register, PageNotFound, Welcome, RegistrationSuccess,Chat } from './containers';
 
 const privateRoutes = [
   {
     id: 'welcome',
     path: '/welcome',
     component: Welcome
+  },
+  {
+    id: 'chat',
+    path: '/chat',
+    component:  Chat
   }
 ];
 
