63b0254f7526dfe568c1eada3ed83b8552beae88
Hugo <hugorales@hotmail.es>
2021-04-15 16:30:10 +0200

    Añadida gestion amigos pod

--- webapp/src/containers/index.js
+++ webapp/src/containers/index.js
@@ -2,15 +2,9 @@ import Login from './Login';
 import Register, { RegistrationSuccess } from './Register';
 import PageNotFound from './PageNotFound';
 import Welcome from './Welcome';
-<<<<<<< HEAD
 import Chat from './Chat'
 import GoogleMap from './GoogleMap';
 import Locations from './Locations';
-
-export { Login, Register, RegistrationSuccess, PageNotFound, Welcome, GoogleMap, Locations, Chat};
-
-=======
 import friends from './friends'
 
-export { Login, Register, RegistrationSuccess, PageNotFound, Welcome, friends };
->>>>>>> feature/rama_hugo
+export { Login, Register, RegistrationSuccess, PageNotFound, Welcome, GoogleMap, Locations, Chat,friends};
