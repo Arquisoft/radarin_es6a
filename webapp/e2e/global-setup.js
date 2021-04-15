0e1e762bd4a81ef1ae9191ff2131f6d66d17b2b9
uo264254 <uo264254@uniovi.es>
2021-04-15 13:50:19 +0200

    test:e2e por defecto

--- webapp/e2e/global-setup.js
+++ webapp/e2e/global-setup.js
@@ -1,7 +1,7 @@
 const { setup: setupDevServer } = require("jest-dev-server")
 module.exports = async () => {
     await setupDevServer([
-    /*{
+    {
         command: 'node start-db.js',
         launchTimeout: 100000,
         debug:true,
@@ -12,7 +12,7 @@ module.exports = async () => {
         launchTimeout: 60000,
         debug:true,
         port: 5000,
-    },*/
+    },
     {
         command: 'npm start',
         launchTimeout: 60000,
