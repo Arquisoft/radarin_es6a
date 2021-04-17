bbd6b58d6b608c8e61f26cc04748a058e6d99d61
UO264699 <Andresfdez9@gmail.com>
2021-04-17 11:08:29 +0200

    chatHelper enviar mensajes

--- webapp/src/containers/Chat/chatHelper.js
+++ webapp/src/containers/Chat/chatHelper.js
@@ -6,15 +6,17 @@ class ChatHelper {
 		return await response.json()
 	}
 
-	async sendMessages(email1,email2) {
+    async sendMessages(email1,email2) {
 		const requestOptions = {
-			method: 'POST'
+			method: 'INSERT'
 		  };
 		const apiEndPoint= process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
 		console.log(apiEndPoint )
 		let response = await fetch(apiEndPoint+'/chat/' +email1+ "/" + email2, requestOptions)
 		return await response.json()
 	}
+
+	
 }
 const chatHelper = new ChatHelper();
 
