bc30ec2c528cfccc1c2c4d43ad0355b8cc29126b
Hugo <hugorales@hotmail.es>
2021-04-26 21:28:38 +0200

    prueba

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -7,9 +7,6 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import Routes from './routes';
 import theme from './utils/theme';
-
-import 'flag-icon-css/css/flag-icon.min.css';
-import 'normalize.css';
 import './index.css';
 import '@inrupt/solid-style-guide';
 import { Toaster } from './App.styled';
