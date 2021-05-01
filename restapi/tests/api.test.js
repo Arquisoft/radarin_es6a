7eb583a8f4c0696a345f7b919d770ed6a5df5206
uo271060 <uo271060@uniovi.es>
2021-05-01 12:01:47 +0200

    Update api.test.js

--- restapi/tests/api.test.js
+++ restapi/tests/api.test.js
@@ -13,7 +13,7 @@ beforeAll(async () => {
  * Remove and close the db and server.
  */
 afterAll(async () => {
-    await server.clearDatabase() 
+    await server.clearDatabase()
     await server.closeServer() //finish the server
     await server.closeDB()
 })
@@ -25,7 +25,7 @@ describe('user ', () => {
     /**
      * Test that we can list users without any error.
      */
-    it('can be listed',async () => {
+    it('can be listed', async () => {
         const response = await request(app).get("/api/users/list");
         expect(response.statusCode).toBe(200);
     });
@@ -34,9 +34,9 @@ describe('user ', () => {
      * Tests that a user can be created through the productService without throwing any errors.
      */
     it('can be created correctly', async () => {
-        idp = 'inrupt.net'
+        idp = 'https://inrupt.net'
         email = 'pablo'
-        const response = await request(app).post('/api/users/add').send({idp: idp,email: email}).set('Accept', 'application/json')
+        const response = await request(app).post('/api/users/add').send({ idp: idp, email: email }).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
         expect(response.body.idp).toBe(idp);
         expect(response.body.email).toBe(email);
@@ -45,11 +45,11 @@ describe('user ', () => {
     /**
      * Tests that the same user can not be inserted.
      */
-     it('can not be created correctly', async () => {
-        idp = 'inrupt.net'
+    it('can not be created correctly', async () => {
+        idp = 'https://inrupt.net'
         email = 'pablo'
         err = "Error: This user is already registered"
-        const response = await request(app).post('/api/users/add').send({idp: idp,email: email}).set('Accept', 'application/json')
+        const response = await request(app).post('/api/users/add').send({ idp: idp, email: email }).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
         expect(response.body.error).toBe(err);
     });
@@ -58,7 +58,7 @@ describe('user ', () => {
      * Tests that a user can be deleted without throwing any errors.
      */
     it('can be deleted correctly', async () => {
-        idp = 'inrupt.net'
+        idp = 'https://inrupt.net'
         email = 'pablo'
         const response = await request(app).get('/api/users/delete/pablo');
         expect(response.statusCode).toBe(200);
@@ -67,8 +67,8 @@ describe('user ', () => {
     /**
      * Tests that a user can not be deleted because user does not exist.
      */
-     it('can not be deleted correctly', async () => {
-        idp = 'inrupt.net'
+    it('can not be deleted correctly', async () => {
+        idp = 'https://inrupt.net'
         email = 'pablo'
         err = "Error: El usuario no está registrado"
         const response = await request(app).get('/api/users/delete/pablo');
@@ -79,10 +79,10 @@ describe('user ', () => {
     /**
      * Remains Pablo in the DB to do next tests
      */
-     it('can be created correctly', async () => {
-        idp = 'inrupt.net'
+    it('can be created correctly', async () => {
+        idp = 'https://inrupt.net'
         email = 'pablo'
-        const response = await request(app).post('/api/users/add').send({idp: idp, email: email}).set('Accept', 'application/json')
+        const response = await request(app).post('/api/users/add').send({ idp: idp, email: email }).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
         expect(response.body.idp).toBe(idp);
         expect(response.body.email).toBe(email);
@@ -92,11 +92,11 @@ describe('user ', () => {
 /**
  * LOCATION test suite.
  */
- describe('location ', () => {
+describe('location ', () => {
     /**
      * Test that we can list locations without any error.
      */
-    it('can be listed',async () => {
+    it('can be listed', async () => {
         const response = await request(app).get("/api/locations/list");
         expect(response.statusCode).toBe(200);
     });
@@ -105,16 +105,18 @@ describe('user ', () => {
      * Tests that a location can be created without throwing any errors.
      * pablo is the email
      */
-     it('can be created correctly', async () => {
+    it('can be created correctly', async () => {
         longitud = -6.23277;
         latitud = 34.23;
         fecha = '2021-03-21T16:20:40.511Z';
         email = 'pablo';
         const response = await request(app).post('/api/locations/add')
-                                .send({longitud: longitud,
-                                    latitud: latitud, 
-                                    fecha: fecha, 
-                                    email: email}).set('Accept', 'application/json')
+            .send({
+                longitud: longitud,
+                latitud: latitud,
+                fecha: fecha,
+                email: email
+            }).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
         expect(response.body.longitud).toBe(longitud);
         expect(response.body.latitud).toBe(latitud);
