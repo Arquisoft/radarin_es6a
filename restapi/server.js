dd7edc91f4cb6ec42b90d82e38bf1631ecfad5d8
uo264254 <uo264254@uniovi.es>
2021-05-06 10:17:44 +0200

    change env: API_URI en deploy-webapp

--- restapi/server.js
+++ restapi/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const promBundle = require("express-prom-bundle");
-const cors = require('cors');
+const  = require('cors');
 const mongoose = require("mongoose")
 const api = require("./api")
 const MAX_LOCATIONS = 5
